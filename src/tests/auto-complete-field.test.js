4caa4b9e05b3de4e4bcac666f00ef9f432fddae1
Narendra Kurapati <narendra.kurapati@hertshtengroup.com>
2018-03-14 14:54:21 +0530

    Unit testing structure

---
+++ src/tests/auto-complete-field.test.js
@@ -0,0 +1,19 @@
+describe('Autocomplete - ', () => {
+    it('Renders without crashing', () => {
+        //Check rendering
+    });
+
+    describe('On input change - ', () => {
+        //Get input
+
+        it('Check for minium characters', done => {
+            //Set input value < minchars
+            //Trigger change & validate results
+        })
+
+        it('Get Suggetions', done => {
+            //Set input value >= minchars
+            //Trigger change & validate results
+        })
+    });
+})
---
+++ src/tests/suggestions-service.test.js
@@ -0,0 +1,9 @@
+describe('Suggestions Servie ', () => {
+    it('Get Suggetions', () => {
+        //Test get suggetions api
+    });
+
+    it('Get Results', () => {
+        //Test get suggetions api
+    });
+})
